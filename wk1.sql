4c64d9624a850d6d486e9d6f8e4162e1e9685200
Eric Born <eric.born85@gmail.com>
2020-01-23 19:42:13 -0800

    Update wk1.sql
    part 2

--- wk1.sql
+++ wk1.sql
@@ -18,4 +18,23 @@ SET DVDAtTime = (SELECT LEFT(MembershipType,1)
 				 WHERE membership.MembershipId = m2.MembershipId
 				)
 
--- d.
+-- d.	Select all data from the membership table to verify that you obtained the intended results.
+SELECT MembershipId, MembershipType, MembershipLimitPerMonth, MembershipMonthlyPrice, 
+	   MembershipMonthlyTax, MembershipDVDLostPrice, DVDAtTime
+FROM membership;
+
+--2. Sequences
+SELECT * FROM rental
+
+-- a.	Implement a single sequence 
+-- Starting the squence at 11 since there are already 10 records inside the rental table
+CREATE SEQUENCE RentalId
+AS INT
+START WITH 11
+INCREMENT BY 1;
+
+-- c.	Demonstrate sequence by inserting two new records into the rental table.
+-- calls next value for from the RentalID sequence as the RentalId column of the rental table
+INSERT INTO rental (RentalId, MemberId, DVDId, RentalRequestDate, RentalShippedDate, RentalReturnedDate)
+VALUES (NEXT VALUE FOR dbo.RentalId, 9, 3, GETDATE(), NULL, NULL),
+	   (NEXT VALUE FOR dbo.RentalId, 8, 4, GETDATE(), NULL, NULL);
