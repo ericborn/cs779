17d26de6c0095a0ce0877ce6e3ba9884a3ae4bc0
Eric Born <eric.born85@gmail.com>
2020-02-07 11:37:48 -0800

    Update rental queue delete.sql

--- rental queue delete.sql
+++ rental queue delete.sql
@@ -1,3 +1,5 @@
+USE NetFlix
+
 --Reset RentalQueue test data.
 DELETE FROM RentalQueue
 WHERE MemberId = 1;
@@ -20,47 +22,48 @@ WHERE MemberId = 1;
 -- Performs error checking on the following: 
 -- Invalid DVDId's.
 -- DVDId not in the members queue.
+-- Member does not have any items in their queue
+CREATE OR ALTER PROCEDURE DELETE_RENTAL_QUEUE
+	@member_id NUMERIC(12,0),
+	@dvd_id NUMERIC(16,0)  
+AS
+DECLARE @queue_position SMALLINT
 
---CREATE OR ALTER PROCEDURE DELETE_RENTAL_QUEUE
---	@member_id NUMERIC(12,0),
---	@dvd_id NUMERIC(16,0)
---AS
--- Create variables to store the highest value current in the queue and that value plus 1
-DECLARE
-		-- Test variables
-		@member_id NUMERIC(12,0),
-		@dvd_id NUMERIC(16,0),
-		@queue_position SMALLINT
+--		-- Test variables
+--DECLARE
+--		@member_id NUMERIC(12,0),
+--		@dvd_id NUMERIC(16,0),
+--		@queue_position SMALLINT
 	
-	-- Set test variables
-	SELECT @member_id = 1,
-		   @dvd_id = 3
+--	-- Set test variables
+--	SELECT @member_id = 1,
+--		   @dvd_id = 3
 
 -- Check to see if member has an established queue by selecting top 1 row from
 -- RentalQueue where MemberId equals @member_id
--- If they have a queue proceed to the next check
+-- If they have a queue proceed to the next check, else throw and error and stop
 IF @member_id IN (SELECT TOP 1 MemberId FROM RentalQueue WHERE MemberId = @member_id)
 	BEGIN
-		-- Check to ensure the DVDId is valid
+		-- Check to ensure the DVDId is valid, else throw and error and stop
 		IF @dvd_id IN (SELECT DVDId FROM DVD) 
 			BEGIN
-				-- If the DVDId is currently in the rental queue for this customer proceed
-				-- if its not, throw an error and stop
+				-- If the DVDId is currently in the rental queue for this customer proceed,
+				-- else throw an error and stop
 				IF @dvd_id IN (SELECT DVDId FROM RentalQueue WHERE MemberId = @member_id)
 					BEGIN
+					-- Find the dvd's current queue position and set a variable with it
 					SET @queue_position = (SELECT QueuePosition 
 										   FROM RentalQueue 
 										   WHERE MemberId = @member_id AND DVDId = @dvd_id)
 						
+						-- Delete from the queue
 						DELETE FROM RentalQueue
-						WHERE MemberId = @member_id AND QueuePosition = @queue_position;
+						WHERE MemberId = @member_id AND @dvd_id = DVDId;
 						
+						-- Update all queue items that had a greater queue value than the dvd that was deleted
 						UPDATE RentalQueue
 						SET QueuePosition = QueuePosition - 1
 						WHERE MemberId = @member_id AND QueuePosition > @queue_position;
-
-						--SELECT * FROM RentalQueue
-						--WHERE MemberId = 1 AND QueuePosition > 5;
 					END;
 				-- Raise an error if the DVD is not in the queue
 				ELSE
@@ -68,7 +71,7 @@ IF @member_id IN (SELECT TOP 1 MemberId FROM RentalQueue WHERE MemberId = @membe
 						RAISERROR('Error, please choose a DVD that is currently in your queue.',11,1);
 					END;
 			END;
-		-- Raise an error for the DVDId being invalid
+		-- Raise an error if the DVDId is invalid
 		ELSE
 			BEGIN
 				RAISERROR('Error, please choose a valid DVDId.',11,1);
