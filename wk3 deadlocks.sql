00f0cf2ba6273792b04c1db39477de72a50500ed
Eric Born <eric.born85@gmail.com>
2020-02-09 18:18:49 -0800

    Update wk3 deadlocks.sql

--- wk3 deadlocks.sql
+++ wk3 deadlocks.sql
@@ -13,9 +13,12 @@ INSERT INTO Deadlocker1 VALUES(2);
 INSERT INTO Deadlocker2 VALUES(1);
 INSERT INTO Deadlocker2 VALUES(2);
 
-SELECT * FROM Deadlocker1
-SELECT * FROM Deadlocker2
+SELECT * FROM Deadlocker1;
+SELECT * FROM Deadlocker2;
 
 -- 4.
 BEGIN TRAN TRAN1
 UPDATE Deadlocker1 SET x=3 where x=1;
+
+-- 7.
+UPDATE Deadlocker2 SET x=5 where x=2; 
