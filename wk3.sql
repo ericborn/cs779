ebc65341458157ea106ddac6ec1da4b16bb6c80f
Eric Born <eric.born85@gmail.com>
2020-02-09 19:00:08 -0800

    Update wk3.sql

--- wk3.sql
+++ wk3.sql
@@ -9,6 +9,32 @@ Homework wk. 3
 -- Function that returns DVDId of the next in stock movie from the customers queue
 -- Takes customer ID as an input and returns the DVDId
 -- Returns NULL if there is no DVD's in stock or the queue is empty
+-- Performs error checking that the customers balance isnt negative
+
+-- Add balance column to the member table
+ALTER TABLE member
+ADD Balance INT;
+
+-- update member 1 with a negative balance
+UPDATE member
+SET Balance = -25
+WHERE MemberId = 1
+
+SELECT * FROM Member
+
+
+-- Check for a balance greater than or equal to 0
+IF (SELECT Balance FROM member WHERE MemberId = @member_id) >= 0
+	BEGIN
+
+	END
+
+-- If members balance is negative throw an error and stop
+ELSE
+	BEGIN
+		RAISERROR('Error, you currently have an outstanding balance.',12,1,@queue_maxPlus);
+	END;
+
 
 SELECT * --DVDId 
 FROM RentalQueue
