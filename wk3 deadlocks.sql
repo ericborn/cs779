c051cbaab29bd918cef33b175d4fdebee7e65768
Eric Born <eric.born85@gmail.com>
2020-02-09 17:54:20 -0800

    Create wk3 deadlocks.sql

---
+++ wk3 deadlocks.sql
@@ -0,0 +1,21 @@
+
+
+--DROP TABLE Deadlocker1
+--DROP TABLE Deadlocker2
+
+-- 2.
+CREATE TABLE Deadlocker1 (x INTEGER);
+CREATE TABLE Deadlocker2 (x INTEGER);
+
+-- 3.
+INSERT INTO Deadlocker1 VALUES(1);
+INSERT INTO Deadlocker1 VALUES(2);
+INSERT INTO Deadlocker2 VALUES(1);
+INSERT INTO Deadlocker2 VALUES(2);
+
+SELECT * FROM Deadlocker1
+SELECT * FROM Deadlocker2
+
+-- 4.
+BEGIN TRAN TRAN1
+UPDATE Deadlocker1 SET x=3 where x=1;
