c80af2f8e039026f08b81890cab83f00ada6b7dc
Eric Born <eric.born85@gmail.com>
2020-02-09 19:34:11 -0800

    Update wk3.sql

--- wk3.sql
+++ wk3.sql
@@ -18,22 +18,31 @@ ADD Balance INT;
 -- update member 1 with a negative balance
 UPDATE member
 SET Balance = -25
-WHERE MemberId = 1
+WHERE MemberId = 1;
 
-SELECT * FROM Member
+SELECT * FROM Member;
 
+CREATE FUNCTION GetNextDVD (
+	@MemberId NUMERIC(12,0)
+)
+RETURNS NUMERIC(12,0)
+AS
+BEGIN
+	-- Check for a balance greater than or equal to 0
+	IF (SELECT Balance FROM member WHERE MemberId = @MemberId) >= 0
+		BEGIN
+			-- Needs to find the lowest queued dvd that is also on hand = 1
+			SELECT TOP 1 dc.DVDId
+			FROM DVD_Copy dc
+			JOIN RentalQueue r ON r.DVDId = dc.DVDId
+			WHERE dc.DVDQtyOnHand = 1 AND r.QueuePosition = (SELECT MIN(QueuePosition) FROM RentalQueue)
+		END
 
--- Check for a balance greater than or equal to 0
-IF (SELECT Balance FROM member WHERE MemberId = @member_id) >= 0
-	BEGIN
-
-	END
-
--- If members balance is negative throw an error and stop
-ELSE
-	BEGIN
-		RAISERROR('Error, you currently have an outstanding balance.',12,1,@queue_maxPlus);
-	END;
+	-- If members balance is negative throw an error and stop
+	ELSE
+		BEGIN
+			RAISERROR('Error, you currently have an outstanding balance.',12,1,@queue_maxPlus);
+		END;
 
 
 SELECT * --DVDId 
