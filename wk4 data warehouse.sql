3545cb79fb82d12a8eb6ce47f8efea4f68bfc1ff
Eric Born <eric.born85@gmail.com>
2020-02-18 13:48:30 -0800

    Update wk4 data warehouse.sql

--- wk4 data warehouse.sql
+++ wk4 data warehouse.sql
@@ -4,9 +4,14 @@ JOIN DVD d ON d.dvd_key = r.dvd_key
 JOIN time t ON t.time_key = r.time_key
 JOIN distribution_center dc ON dc.distribution_center_key = r.distribution_center_key
 WHERE t.year BETWEEN 2017 AND 2019
-GROUP BY d.genre, t.year, r.dvd_on_rent
+GROUP BY d.genre, t.year
 
-SELECT t.month, t.year, COUNT(m.time_key)
+SELECT t.month, COUNT(m.member_id) AS 'New Subscriptions'
 FROM time t
 JOIN member m ON m.time_key = t.time_key
-GROUP BY 
+GROUP BY t.month
+
+SELECT ml.state, COUNT(m.member_id) AS 'Total Members'
+FROM members m
+JOIN member_location ml ON ml.location_key = m.location_key
+GROUP BY ml.state
