6c95ea96c5705e49b1d30ce74ea2ff972299599f
Eric Born <eric.born85@gmail.com>
2020-02-19 21:07:40 -0800

    Update Final.sql

--- Final.sql
+++ Final.sql
@@ -16,6 +16,20 @@ EXEC sp_rename 'customers.["customer_zip_code_prefix"]', 'customer_zip_code_pref
 EXEC sp_rename 'customers.["customer_city"]', 'customer_city'
 EXEC sp_rename 'customers.["customer_state"]', 'customer_state'
 
+-- script to output database schema
+-- Provided by lucidchart.com with their import data feature
+SELECT 'sqlserver' dbms,t.TABLE_CATALOG,t.TABLE_SCHEMA,t.TABLE_NAME,c.COLUMN_NAME,c.ORDINAL_POSITION,c.DATA_TYPE,
+c.CHARACTER_MAXIMUM_LENGTH,n.CONSTRAINT_TYPE
+FROM INFORMATION_SCHEMA.TABLES t 
+LEFT JOIN INFORMATION_SCHEMA.COLUMNS c ON t.TABLE_CATALOG=c.TABLE_CATALOG AND t.TABLE_SCHEMA=c.TABLE_SCHEMA AND t.TABLE_NAME=c.TABLE_NAME 
+LEFT JOIN(INFORMATION_SCHEMA.KEY_COLUMN_USAGE k 
+JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS n ON k.CONSTRAINT_CATALOG=n.CONSTRAINT_CATALOG 
+AND k.CONSTRAINT_SCHEMA=n.CONSTRAINT_SCHEMA AND k.CONSTRAINT_NAME=n.CONSTRAINT_NAME 
+LEFT JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS r ON k.CONSTRAINT_CATALOG=r.CONSTRAINT_CATALOG AND k.CONSTRAINT_SCHEMA=r.CONSTRAINT_SCHEMA 
+AND k.CONSTRAINT_NAME=r.CONSTRAINT_NAME)ON c.TABLE_CATALOG=k.TABLE_CATALOG AND c.TABLE_SCHEMA=k.TABLE_SCHEMA AND c.TABLE_NAME=k.TABLE_NAME AND c.COLUMN_NAME=k.COLUMN_NAME 
+WHERE t.TABLE_TYPE='BASE TABLE';
+
+-- Code section to clean any issues found in the data
 -- Find if all zips are the same length
 SELECT DISTINCT LEN(customer_zip_code_prefix)
 FROM customers
@@ -30,13 +44,39 @@ UPDATE customers
 SET customer_zip_code_prefix = (SELECT RIGHT(LEFT(customer_zip_code_prefix, 6),5)
 								FROM customers c
 								WHERE customers.customer_id = c.customer_id)
- 
-SELECT * FROM customers
 
--- find misspelled Sãu Paulo in seller_city column
+-- Find misspelled Sãu Paulo in seller_city column
 SELECT distinct seller_city
 FROM sellers s
 WHERE seller_city LIKE 'sao pau%' OR seller_city LIKE 'sao palu%'
+
+-- Replace misspellings
+UPDATE sellers
+SET seller_city = 'Sãu Paulo'
+WHERE seller_city LIKE 'sao pau%' OR seller_city LIKE 'sao palu%'
+
+
+----------------------------
+-- Purchases from Sept 4 2016 to Oct 17 2018
+SELECT MIN(Order_purchase_timestamp) AS 'First', 
+MAX(Order_purchase_timestamp) AS 'Last'
+FROM orders o
+
+-- Gathers the data for the orders table
+SELECT TOP 100 o.order_purchase_timestamp, c.product_category_name_english, oi.seller_id, s.seller_city, 
+s.seller_state, SUM(oi.price) AS 'Total_Value', COUNT(oi.product_id) AS 'Units_Sold'
+FROM orders o
+JOIN order_items oi ON oi.order_id = o.order_id
+JOIN products p ON p.product_id = oi.product_id
+JOIN category c ON c.product_category_name = p.product_category_name
+JOIN sellers s ON s.seller_id = oi.seller_id
+GROUP BY o.order_purchase_timestamp, c.product_category_name_english, oi.seller_id, s.seller_city, 
+s.seller_state
+ 
+
+
+
+SELECT * FROM customers
 
 SELECT top 100 *
 FROM sellers s
@@ -50,7 +90,7 @@ JOIN order_items oi ON oi.order_id = o.order_id
 JOIN products p ON p.product_id = oi.product_id
 JOIN category c ON c.product_category_name = p.product_category_name
 
-SELECT count(*) --TOP 100 *
+SELECT TOP 100 *
 FROM order_items --orders
 
 SELECT TOP 100 *--count(*)
